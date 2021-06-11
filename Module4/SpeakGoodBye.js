2c8d48afdf8c4fea23a70f4cfe7fb5101aef691a
cenofett <cenofett@gmail.com>
2021-06-11 12:15:35 -0700

    Create SpeakGoodBye.js

---
+++ Module4/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function (window) {
+
+    var speakWord = "Good Bye";
+    var byeSpeaker = function speak(name) {
+        console.log(speakWord + " " + name);
+    }
+
+    window.byeSpeaker = byeSpeaker;
+
+})(window);
+
